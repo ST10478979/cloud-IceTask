a0882dd1f3492c1f420969c766ca694de6eacac9
ST10478979 <st10478979@myemeris.edu.za>
2026-03-25 08:34:19 +0200

    Models

---
+++ MechanicShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MechanicShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MechanicShop/Models/Service.cs
@@ -0,0 +1,13 @@
+﻿namespace MechanicShop.Models
+{
+    public class Service
+    {
+
+        public DateTime BookingDate { get; set; }
+        public int LabourHours { get; set; }
+
+        public string MechanicName { get; set; }
+
+
+    }
+}
